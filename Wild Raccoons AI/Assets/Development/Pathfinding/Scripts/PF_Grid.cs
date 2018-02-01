8c922013075522205537a45e054c3e192bb3123a
Ryan Dougherty <Ryandoca23@gmail.com>
2018-02-01 05:39:55 +0000

    Pathfinding - Grid Setup

---
+++ Wild Raccoons AI/Assets/Development/Pathfinding/Scripts/PF_Grid.cs
@@ -0,0 +1,101 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PF_Grid : MonoBehaviour
+{
+    public Transform player;
+
+    public LayerMask unwalkableMask;
+    public Vector2 gridWorldSize = Vector2.zero;
+    public float nodeRadius = 0f;
+    private PF_Node[,] grid;
+
+    private float nodeDiameter = 1f;
+    private int gridSizeX = 0;
+    private int gridSizeY = 0;
+
+    private void Start()
+    {
+        nodeDiameter = nodeRadius * 2f;
+        gridSizeX = Mathf.RoundToInt(gridWorldSize.x / nodeDiameter);
+        gridSizeY = Mathf.RoundToInt(gridWorldSize.y / nodeDiameter);
+
+        // Generate the nodes which will comprise the grid.
+        CreateGrid();
+    }
+
+    private void CreateGrid()
+    {
+        grid = new PF_Node[gridSizeX, gridSizeY];
+
+        // Get the world position for the bottom left of the grid.
+        Vector3 worldBottomLeft = transform.position - Vector3.right * gridWorldSize.x / 2 - Vector3.forward * gridWorldSize.y / 2;
+
+        Debug.Log(worldBottomLeft);
+
+        for (int x = 0; x < gridSizeX; x++)
+        {
+            for(int y = 0; y < gridSizeY; y++)
+            {
+                // Get the world position for the current node.
+                Vector3 worldPoint = worldBottomLeft + Vector3.right * (x * nodeDiameter + nodeRadius) + Vector3.forward * (y * nodeDiameter + nodeRadius);
+
+                // Determine if the node is walkable or should be considered obstructed.
+                bool walkable = !(Physics.CheckSphere(worldPoint, nodeRadius, unwalkableMask));
+
+                // Update the node information within the grid for this particular node.
+                grid[x, y] = new PF_Node(walkable, worldPoint);
+            }
+        }
+    }
+
+    public PF_Node GetNodeFromWorldPoint(Vector3 _worldPos)
+    {
+        // Get the world position as a percentage of the entire grid.
+        float percentX = (_worldPos.x - transform.position.x + gridWorldSize.x / 2) / gridWorldSize.x;
+        float percentY = (_worldPos.z - transform.position.z + gridWorldSize.y / 2) / gridWorldSize.y;
+
+        percentX = Mathf.Clamp01(percentX);
+        percentY = Mathf.Clamp01(percentY);
+
+        int x = Mathf.RoundToInt((gridSizeX - 1) * percentX);
+        int y = Mathf.RoundToInt((gridSizeY - 1) * percentY);
+
+        return grid[x, y];
+    }
+
+    private void OnDrawGizmos()
+    {
+        // Wireframe to show grid boundaries in editor.
+        Gizmos.DrawWireCube(transform.position, new Vector3(gridWorldSize.x, 1, gridWorldSize.y));
+
+        if(grid != null)
+        {
+            PF_Node playerNode = GetNodeFromWorldPoint(player.position);
+
+            foreach(PF_Node n in grid)
+            {
+                if(n.walkable)
+                {
+                    // Walkable.
+                    Gizmos.color = Color.white;
+                }
+                else
+                {
+                    // Obstacle.
+                    Gizmos.color = Color.red;
+                }
+
+                if(playerNode == n)
+                {
+                    // Player
+                    Gizmos.color = Color.cyan;
+                }
+
+                // Cubes to represent node positions.
+                Gizmos.DrawCube(n.worldPos, Vector3.one * (nodeDiameter - 0.1f));
+            }
+        }
+    }
+}
---
+++ Wild Raccoons AI/Assets/Development/Pathfinding/Scripts/PF_Node.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PF_Node
+{
+    public bool walkable = false;
+    public Vector3 worldPos = Vector3.zero;
+
+    public PF_Node(bool _walkable, Vector3 _worldPos)
+    {
+        walkable = _walkable;
+        worldPos = _worldPos;
+    }
+}
